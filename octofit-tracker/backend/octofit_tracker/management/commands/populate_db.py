c90637643d61fc5bea2cf4eb4004d57312eeacbe
denyssergieiev <denys.sergieiev@accenture.com>
2025-10-28 10:46:39 +0000

    step 3

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,66 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from djongo import models
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Activity(models.Model):
+    user = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Leaderboard(models.Model):
+    team = models.CharField(max_length=100)
+    points = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    difficulty = models.CharField(max_length=50)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        # Delete all data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        ironman = User.objects.create_user(username='ironman', email='ironman@marvel.com', password='pass')
+        captain = User.objects.create_user(username='captain', email='captain@marvel.com', password='pass')
+        batman = User.objects.create_user(username='batman', email='batman@dc.com', password='pass')
+        superman = User.objects.create_user(username='superman', email='superman@dc.com', password='pass')
+
+        # Create activities
+        Activity.objects.create(user='ironman', type='run', duration=30)
+        Activity.objects.create(user='captain', type='cycle', duration=45)
+        Activity.objects.create(user='batman', type='swim', duration=60)
+        Activity.objects.create(user='superman', type='run', duration=50)
+
+        # Create leaderboard
+        Leaderboard.objects.create(team='Marvel', points=75)
+        Leaderboard.objects.create(team='DC', points=110)
+
+        # Create workouts
+        Workout.objects.create(name='Pushups', difficulty='Easy')
+        Workout.objects.create(name='Squats', difficulty='Medium')
+        Workout.objects.create(name='Deadlift', difficulty='Hard')
+
+        self.stdout.write(self.style.SUCCESS('octofit_db populated with test data'))
